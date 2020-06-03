26d976acbf318517917c42bb63d40c8faba09c52
meditch05 <meditch05@gmail.com>
2020-06-03 10:24:31 +0900

    1

--- src/main/java/com/mw/enpharos/service/TpsResDTOService.java
+++ src/main/java/com/mw/enpharos/service/TpsResDTOService.java
@@ -11,6 +11,11 @@ import org.springframework.stereotype.Service;
 
 import com.mw.enpharos.dto.TpsResDTO;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 @Service
 public class TpsResDTOService {
 	
