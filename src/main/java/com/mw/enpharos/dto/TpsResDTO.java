185ffb4ba7a79df4b42ca4689843f0e22cb62b11
meditch05 <meditch05@gmail.com>
2020-06-03 10:55:36 +0900

    1

--- src/main/java/com/mw/enpharos/dto/TpsResDTO.java
+++ src/main/java/com/mw/enpharos/dto/TpsResDTO.java
@@ -1,26 +1,23 @@
 package com.mw.enpharos.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+
+import java.math.BigDecimal;
 
 @Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
 public class TpsResDTO {
-    private String  time;
-    private float	tps;
-    private float	res;
-    
-    @Override
-    public String toString() {
-        return "TpsResDTO{"        		
-                + "time = " + time
-                + ", tps=" + tps
-                + ", res=" + res
-                + "}";
+	private String time;
+	private float tps;
+	private float res;
+
+	public TpsResDTO(String  time, BigDecimal	tps, BigDecimal	res) {
+    	this.time = time;
+    	this.tps = tps.floatValue();
+    	this.res = res.floatValue();
     }
+
+	@Override
+	public String toString() {
+		return "TpsResDTO{" + "time = " + time + ", tps=" + tps + ", res=" + res + "}";
+	}
 }
