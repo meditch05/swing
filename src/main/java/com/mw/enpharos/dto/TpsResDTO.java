33ad2928ef94fa00133ca479897b1e31cb04eb91
meditch05 <meditch05@gmail.com>
2020-06-03 10:39:26 +0900

    2

--- src/main/java/com/mw/enpharos/dto/TpsResDTO.java
+++ src/main/java/com/mw/enpharos/dto/TpsResDTO.java
@@ -12,12 +12,6 @@ public class TpsResDTO {
     private float	tps;
     private float	res;
     
-    public TpsResDTO(String time, float tps, float res) {
-    	this.time = time;
-    	this.tps = tps;
-    	this.res = res;
-    }
-    
     @Override
     public String toString() {
         return "TpsResDTO{"        		
